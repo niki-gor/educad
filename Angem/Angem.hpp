fbe83a366b7058b2955731e1ae8e3e59a8a066c2
rkhazh <dev.null_r@mail.ru>
2022-04-15 22:28:05 +0000

    Change angem

--- Angem/Angem.hpp
+++ Angem/Angem.hpp
@@ -6,26 +6,38 @@ class AngemEntity {
 };
 
 class AngemPoint {
-private:
+public:
     double x;
     double y;
     double z;
 };
 
 class AngemLine {
-private:
-
-
-
+public:
+    // i j k -- направляющий вектор
+    double i;
+    double j;
+    double k;
     double x0;
     double y0;
     double z0;
 };
 
 class AngemPlane {
-private:
+public:
     double A;
     double B;
     double C;
     double D;
 };
+
+class AngemUtils {
+public:
+    float getDistance(AngemPoint p1, AngemPoint p2){
+        return 0;
+    }
+    AngemLine getPerpendicularLine(AngemPoint p, AngemLine l){
+        return AngemLine();
+    }
+};
+
