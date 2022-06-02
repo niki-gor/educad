9db083bb7fb3ea88ad032e61b13a2604a688c8ac
Marat Gasanov <maratgasanov6@gmail.com>
2022-06-02 15:32:18 +0300

    add link line interface

--- Entity/Entity.hpp
+++ Entity/Entity.hpp
@@ -299,4 +299,5 @@ public:
     virtual void deletePoint(int x, int y, int xBegin, int yBegin, int planeNumber, std::string name) = 0;
     virtual void deleteLine(int x1, int y1, int x2, int y2, int xBegin, int yBegin, int planeNumber, std::string name) = 0;
     virtual void addPlaneByLineAndPoint (int x, int y, int x1, int y1, int x2, int y2, int xBegin, int yBegin, int planeNumber, std::string name, TwoDEntity* twoDEntity)=0;
+    virtual void addLinkLine(PTR<TwoDEntity> entity1, PTR<TwoDEntity> entity2);
 };
